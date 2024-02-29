2fdf66918927c3b67e70a80e5500176c93cdd290
Sheila Lopes Carvalho de Camargo <91272208+SheilaLCC@users.noreply.github.com>
2024-02-29 18:13:24 -0300

    Primeiro Commit
    Primeiro commit da calculadora de gorjeta

---
+++ index.js
@@ -0,0 +1,18 @@
+function update(){
+    let bill = Number(document.getElementById('yourBill').value)
+    let tipPercent = document.getElementById('tipInput').value
+    let split = document.getElementById('splitInput').value
+
+    let tipValue = bill * (tipPercent / 100)
+    let billTotal = bill + tipValue
+    let billEach = billTotal / split
+
+
+    document.getElementById('tipPercent').innerHTML = tipPercent + " %"
+    document.getElementById('tipValue').innerHTML = '$' + tipValue.toFixed(2) 
+    document.getElementById('totalWithTip').innerHTML = '$' + billTotal.toFixed(2) 
+    document.getElementById('splitValue').innerHTML = split 
+    document.getElementById('billEach').innerHTML = (billTotal / split).toFixed(2)
+
+
+}
